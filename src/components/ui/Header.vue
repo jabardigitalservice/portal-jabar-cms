58125b1af4cbbc935ce817658c079cd954cc8cd2
Ibwedagama <wardana.wedagama@gmail.com>
2021-12-23 12:03:17 +0700

    chore (header): add ref to element structure

--- src/components/ui/Header.vue
+++ src/components/ui/Header.vue
@@ -1,6 +1,12 @@
 <template>
-  <header class="w-full h-[72px] sticky top-0 px-6 flex items-center justify-between bg-white">
-    <h1 class="font-roboto text-xl font-bold text-blue-gray-800 mr-4">
+  <header
+    ref="header"
+    class="w-full h-[72px] sticky top-0 px-6 flex items-center justify-between bg-white"
+  >
+    <h1
+      ref="header-title"
+      class="font-roboto text-xl font-bold text-blue-gray-800 mr-4"
+    >
       {{ pageName }}
     </h1>
     <div class="flex gap-6">
