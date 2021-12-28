e57d0f3d6a0a1f94f81d2436080f2af6bbcb3ee2
Ibwedagama <wardana.wedagama@gmail.com>
2021-12-28 14:06:14 +0700

    setup: install portal-vue package; setup portal-vue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import { DesignSystem } from '@jabardigitalservice/jds-design-system';
+import PortalVue from 'portal-vue';
 import store from './store';
 import App from './App';
 import router from './router';
 import './assets/styles/main.css';
 
 Vue.use(DesignSystem);
+Vue.use(PortalVue);
 
 Vue.config.productionTip = false;
 
