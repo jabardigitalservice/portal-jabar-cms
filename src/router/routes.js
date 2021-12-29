04c54a5c0f27a3b14c3bbe2f2508883864f44389
Ibwedagama <wardana.wedagama@gmail.com>
2021-12-29 11:19:11 +0700

    chore (routes): add more routes; add dummy page component

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,26 @@ export default [
     component: () => import('@/pages/HomePage'),
   },
   {
+    path: '/layanan',
+    name: 'Layanan',
+    component: () => import('@/pages/HomePage'),
+  },
+  {
+    path: '/agenda',
+    name: 'Agenda',
+    component: () => import('@/pages/HomePage'),
+  },
+  {
+    path: '/berita-dan-informasi',
+    name: 'Berita dan Informasi',
+    component: () => import('@/pages/HomePage'),
+  },
+  {
+    path: '/pengaturan',
+    name: 'Pengaturan',
+    component: () => import('@/pages/HomePage'),
+  },
+  {
     path: '/login',
     name: 'Login',
     component: () => import('@/pages/LoginPage'),
