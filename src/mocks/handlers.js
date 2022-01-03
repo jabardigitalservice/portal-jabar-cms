f2f6bb8177c70d72b5d701c132403a7cfd4594a3
Bangun Bagustapa <bangunbagustapa@gmail.com>
2022-01-03 15:19:24 +0700

    mock: add login handlers

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,8 +1,31 @@
 /* eslint-disable import/prefer-default-export */
 import { rest } from 'msw';
 
+const BASE_URL = process.env.VUE_APP_API_URL;
+
 const handlers = [
-  rest.get('/login', async (req, res, ctx) => res(ctx.status(200))),
+  rest.post(`${BASE_URL}/v1/auth/login`, (req, res, ctx) => res(
+    ctx.status(401),
+  )),
+
+  // rest.post(`${BASE_URL}/v1/auth/login`, (req, res, ctx) => res(
+  //   ctx.status(200),
+  //   ctx.json({
+  //     access_token: 'asdasdasd',
+  //     refresh_token: '123123123',
+  //     exp: '123123',
+  //   }),
+  // )),
+
+  // rest.get(`${BASE_URL}/v1/auth/me`, (req, res, ctx) => res(
+  //   ctx.status(200),
+  //   ctx.json({
+  //     data: {
+  //       id: '121212',
+  //       username: 'bangun',
+  //     },
+  //   }),
+  // )),
 ];
 
 export { handlers };
