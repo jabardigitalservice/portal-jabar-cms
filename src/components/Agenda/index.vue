f794522b34658a7e13ff0e667c538ba9ba6a7f95
Ibwedagama <wardana.wedagama@gmail.com>
2022-01-04 13:25:55 +0700

    chore (agenda): remove unused wrapper

--- src/components/Agenda/index.vue
+++ src/components/Agenda/index.vue
@@ -1,22 +1,20 @@
 <template>
   <main class="w-full h-full">
-    <div class="w-full h-full">
-      <section class="px-6 py-4 rounded-lg bg-white mb-4">
-        <h1 class="font-roboto font-medium text-[21px] leading-[34px] text-green-700 mb-3">
-          Daftar Semua Agenda
-        </h1>
-        <!-- TODO: Change this description text -->
-        <p class="font-lato text-sm leading-6 text-blue-gray-800">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendrerit euismod non, semper eu interdum habitasse velit. Eu fermentum fames id.
-        </p>
-      </section>
+    <section class="px-6 py-4 rounded-lg bg-white mb-4">
+      <h1 class="font-roboto font-medium text-[21px] leading-[34px] text-green-700 mb-3">
+        Daftar Semua Agenda
+      </h1>
+      <!-- TODO: Change this description text -->
+      <p class="font-lato text-sm leading-6 text-blue-gray-800">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendrerit euismod non, semper eu interdum habitasse velit. Eu fermentum fames id.
+      </p>
+    </section>
 
-      <section class="px-6 py-4 rounded-lg bg-white border-2 border-green-600">
-        <div class="w-full min-h-[500px]">
+    <section class="px-6 py-4 rounded-lg bg-white border-2 border-green-600">
+      <div class="w-full min-h-[500px]">
         <!-- TODO: add Table Filter, Table Body and Pagination -->
-        </div>
-      </section>
-    </div>
+      </div>
+    </section>
   </main>
 </template>
 
