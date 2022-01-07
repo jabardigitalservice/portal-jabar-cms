81fd826ed4fe5c82243a24b618c5c46f63cb52d7
Ibwedagama <wardana.wedagama@gmail.com>
2022-01-07 13:31:12 +0700

    feat (agenda): add pagination functionality

--- src/components/Agenda/AgendaTable.vue
+++ src/components/Agenda/AgendaTable.vue
@@ -4,6 +4,11 @@
       :headers="tableHeader"
       :items="items"
       :loading="loading"
+      :pagination="pagination"
+      @next-page="onPaginationChange('next-page', $event)"
+      @previous-page="onPaginationChange('previous-page', $event)"
+      @per-page-change="onPaginationChange('per-page-change', $event)"
+      @page-change="onPaginationChange('page-change', $event)"
     >
       <!-- eslint-disable-next-line vue/valid-v-slot -->
       <template #item.action="{item}">
@@ -33,11 +38,76 @@ export default {
       require: false,
       default: false,
     },
+    meta: {
+      type: Object,
+      require: false,
+      default: () => ({}),
+    },
   },
   data() {
     return {
       tableHeader: AGENDA_TABLE_HEADER,
+      pagination: {
+        currentPage: 1,
+        itemsPerPage: 10,
+        totalRows: 0,
+        itemsPerPageOptions: [10, 20, 30, 40, 50],
+      },
     };
+  },
+  watch: {
+    meta: {
+      handler() {
+        this.pagination = {
+          ...this.pagination,
+          currentPage: this.meta?.current_page || 1,
+          itemsPerPage: this.meta?.per_page || 10,
+          totalRows: this.meta?.total_count || 0,
+        };
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    onPaginationChange(action, value) {
+      const paginationObj = { ...this.pagination };
+
+      switch (action) {
+        case 'next-page':
+        case 'previous-page':
+        case 'page-change':
+          paginationObj.currentPage = value;
+          break;
+
+        case 'per-page-change':
+          paginationObj.itemsPerPage = value;
+          break;
+
+        default:
+          break;
+      }
+
+      this.pagination = { ...paginationObj };
+
+      /**
+       *  NOTE:
+       *  `jds-pagination` emits `per-page-change` and `page-change` events
+       *  whenever user changes per page value.
+       *
+       *  To avoid double fetch, we immediately stop this function on
+       *  `per-page-change` event and let `page-change` event to
+       *  fetch data from API
+       */
+
+      if (action === 'per-page-change') {
+        return;
+      }
+
+      this.$emit('update:pagination', {
+        page: this.pagination.currentPage,
+        per_page: this.pagination.itemsPerPage,
+      });
+    },
   },
 };
 </script>
