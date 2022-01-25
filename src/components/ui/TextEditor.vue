961ad10110e67b3a26ca86406b3551e9d64d1eb7
Ibwedagama <wardana.wedagama@gmail.com>
2022-01-25 14:08:51 +0700

    style (text-editor): add ipj custom skin; adjust toolbar

--- src/components/ui/TextEditor.vue
+++ src/components/ui/TextEditor.vue
@@ -6,20 +6,21 @@
       cloud-channel="5"
       :init="{
         height: 500,
+        skin_url: '/tinymce-skin-ipj/',
+        language: 'id',
         menubar: false,
-        images_upload_url: 'https://api-staging.com',
         /* we override default upload handler to simulate successful upload*/
-        images_upload_handler: onUpload,
         plugins: [
           'advlist autolink lists link image charmap print preview anchor',
           'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table paste code help wordcount autosave'
+          'insertdatetime media table paste code help wordcount'
         ],
         toolbar:
-          'restoredraft | undo redo | formatselect | bold italic backcolor | \
-           alignleft aligncenter alignright alignjustify | \
-           bullist numlist outdent indent | removeformat | preview | image | link | media | help',
-        autosave_ask_before_unload: true
+          'undo redo | formatselect | bold italic bullist numlist blockquote backcolor | \
+          alignleft aligncenter alignright alignjustify | \
+          outdent indent | link image media | removeformat | preview | ',
+        images_upload_url: 'https://loremipsum.com',
+        images_upload_handler: onUpload
       }"
     />
     <button>
@@ -46,8 +47,9 @@ export default {
       console.log(this.content);
     },
     onUpload(blobInfo, success /** ,failure */) {
+      // console.log(blobInfo.blob());
       setTimeout(() => {
-      /* no matter what you upload, we will turn it into TinyMCE logo :) */
+        // TODO: Create upload function here
         success('http://moxiecode.cachefly.net/tinymce/v9/images/logo.png');
       }, 2000);
     },
