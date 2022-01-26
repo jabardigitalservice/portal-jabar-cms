aa1effb63fafb03b25b66b6a8ed49db2f3727989
Ibwedagama <wardana.wedagama@gmail.com>
2022-01-26 15:24:14 +0700

    feat (repositories): add media repository

--- src/repositories/RepositoryFactory.js
+++ src/repositories/RepositoryFactory.js
@@ -1,9 +1,11 @@
 import authRepository from './authRepository';
 import agendaRepository from './agendaRepository';
+import mediaRepository from './mediaRepository';
 
 const repositories = {
   auth: authRepository,
   agenda: agendaRepository,
+  media: mediaRepository,
 };
 
 export const RepositoryFactory = {
