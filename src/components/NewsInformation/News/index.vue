0a1db0e57d6c4ec3e4233571619589b4db36f918
Ibwedagama <wardana.wedagama@gmail.com>
2022-01-31 12:11:29 +0700

    chore (news-information): create news section component; create news-tab=bar component

---
+++ src/components/NewsInformation/News/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <main class="w-full">
+    <section class="w-full border-2 border-green-600 bg-green-600 rounded-lg overflow-hidden">
+      <!-- TODO: change tab data count with real data -->
+      <NewsTabBar
+        :tabs="tabs"
+        :current-tab.sync="currentTab"
+      />
+      <div class="w-full h-[500px] bg-white" />
+    </section>
+  </main>
+</template>
+
+<script>
+import NewsTabBar from '@/components/NewsInformation/News/NewsTabBar';
+
+export default {
+  name: 'NewsSection',
+  components: {
+    NewsTabBar,
+  },
+  data() {
+    return {
+      tabs: [
+        {
+          key: 'all',
+          label: 'Semua Berita',
+          icon: 'document',
+          count: 80,
+        },
+        {
+          key: 'published',
+          label: 'Diterbitkan',
+          icon: 'publish',
+          count: 20,
+        },
+        {
+          key: 'draft',
+          label: 'Tersimpan',
+          icon: 'draft',
+          count: 15,
+        },
+        {
+          key: 'review',
+          label: 'Menunggu Review',
+          icon: 'review',
+          count: 10,
+        },
+        {
+          key: 'archive',
+          label: 'Diarsipkan',
+          icon: 'archive',
+          count: 10,
+        },
+      ],
+      currentTab: 'all',
+    };
+  },
+};
+</script>
