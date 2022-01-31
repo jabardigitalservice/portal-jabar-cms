609eb501d30f75314aa65595710d58132a64bb7e
Ibwedagama <wardana.wedagama@gmail.com>
2022-01-31 15:31:22 +0700

    chore (constant): add news table header and news status map constant

--- src/common/constants/index.js
+++ src/common/constants/index.js
@@ -83,3 +83,42 @@ export const AGENDA_STATUS_MAP = {
   unpublish: 'Belum Dipublish',
   archive: 'Dibuang',
 };
+
+export const NEWS_TABLE_HEADER = [
+  {
+    key: 'title',
+    text: 'Judul Agenda/Event',
+    sortable: true,
+  },
+  {
+    key: 'category',
+    text: 'Ketegori',
+    sortable: true,
+  },
+  {
+    key: 'author',
+    text: 'Penulis',
+    sortable: true,
+  },
+  {
+    key: 'updated_at',
+    text: 'Terakhir Diupdate',
+    sortable: true,
+  },
+  {
+    key: 'status',
+    text: 'Status',
+    sortable: true,
+  },
+  {
+    key: 'action',
+    text: 'Aksi',
+  },
+];
+
+export const NEWS_STATUS_MAP = {
+  PUBLISHED: 'Diterbitkan',
+  DRAFT: 'Tersimpan',
+  ARCHIVE: 'Diarsipkan',
+  REVIEW: 'Menunggu Review',
+};
