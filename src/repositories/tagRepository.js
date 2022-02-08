693b69de77e5897a8352c454b69b2c2c21e8434f
Bangun Bagustapa <bangunbagustapa@gmail.com>
2022-02-08 12:19:53 +0700

    docs: add docs for get tag suggestions method

--- src/repositories/tagRepository.js
+++ src/repositories/tagRepository.js
@@ -3,6 +3,14 @@ import Repository from './Repository';
 const resource = '/tags';
 
 export default {
+  /**
+   * Get tag suggestions
+   * @param {Object} params
+   *
+   * @property {string} q - filter suggestions by keyword
+   *
+   * @returns {Promise}
+   */
   getTagSuggestions(params = {}) {
     return Repository.get(`${resource}`, { params });
   },
