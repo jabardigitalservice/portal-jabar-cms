153fb3d2332ad30866f11876600abbd681462eb9
Bangun Bagustapa <bangunbagustapa@gmail.com>
2022-02-09 11:18:37 +0700

    feat: add area repository

--- src/repositories/RepositoryFactory.js
+++ src/repositories/RepositoryFactory.js
@@ -1,5 +1,6 @@
 import authRepository from './authRepository';
 import agendaRepository from './agendaRepository';
+import areaRepository from './areaRepository';
 import newsRepository from './newsRepository';
 import mediaRepository from './mediaRepository';
 import tagRepository from './tagRepository';
@@ -7,6 +8,7 @@ import tagRepository from './tagRepository';
 const repositories = {
   auth: authRepository,
   agenda: agendaRepository,
+  area: areaRepository,
   news: newsRepository,
   media: mediaRepository,
   tag: tagRepository,
