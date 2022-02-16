67e0f30a647471e8dbbb4b32b6e83d23cf866e8d
Bangun Bagustapa <bangunbagustapa@gmail.com>
2022-02-16 12:13:35 +0700

    chore: remove unnecessary code

--- src/pages/News/CreateEditNews.vue
+++ src/pages/News/CreateEditNews.vue
@@ -765,12 +765,9 @@ export default {
           maxWidth: 1200,
           maxHeight: 900,
         });
-        console.log(1);
         const fileUri = await this.uploadMedia(compressedImage);
         success(fileUri);
       } catch (err) {
-        console.log(2);
-        console.log({ err });
         // Show error message and remove image from the document
         failure('Gagal menambahkan gambar', { remove: true });
       }
