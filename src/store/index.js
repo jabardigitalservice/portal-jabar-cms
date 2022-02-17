793285261b8b539e8fd988a6272eda3db19d54be
Bangun Bagustapa <bangunbagustapa@gmail.com>
2022-02-17 12:14:49 +0700

    feat: add vue persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,23 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersist from 'vuex-persist';
 import auth from './modules/auth';
 import alert from './modules/alert';
+import news from './modules/news';
 
 Vue.use(Vuex);
+
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex',
+  storage: window.localStorage,
+  reducer: (state) => ({ news: state.news }),
+});
 
 export default new Vuex.Store({
   modules: {
     auth,
     alert,
+    news,
   },
+  plugins: [vuexLocalStorage.plugin],
 });
