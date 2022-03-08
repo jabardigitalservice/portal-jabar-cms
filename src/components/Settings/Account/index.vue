8ff91d7e2d2f2d6fd92b025bb3b9328ce78ee428
Bangun Bagustapa <bangunbagustapa@gmail.com>
2022-03-08 15:58:35 +0700

    refactor: refactor account settings file structure (#101)

---
+++ src/components/Settings/Account/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="space-y-4 pb-4">
+    <PersonalInformationSection />
+  </main>
+</template>
+
+<script>
+import PersonalInformationSection from './PersonalInformationSection';
+
+export default {
+  name: 'AccountSettings',
+  components: {
+    PersonalInformationSection,
+  },
+};
+</script>
