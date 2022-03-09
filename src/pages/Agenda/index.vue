f13d3558e375f5c462a7eb104739b2caaee0435b
Ibwedagama <wardana.wedagama@gmail.com>
2022-03-09 10:46:23 +0800

    refactor (agenda): remove page description

--- src/pages/Agenda/index.vue
+++ src/pages/Agenda/index.vue
@@ -1,15 +1,5 @@
 <template>
   <main class="w-full pb-20">
-    <section class="px-6 py-4 rounded-lg bg-white mb-4">
-      <h1 class="font-roboto font-medium text-[21px] leading-[34px] text-green-700 mb-3">
-        Daftar Semua Agenda
-      </h1>
-      <!-- TODO: Change this description text -->
-      <p class="font-lato text-sm leading-6 text-blue-gray-800">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendrerit euismod non, semper eu interdum habitasse velit. Eu fermentum fames id.
-      </p>
-    </section>
-
     <section class="px-3 py-6 rounded-lg bg-white border-2 border-green-600">
       <div class="w-full">
         <!-- Table Menu -->
