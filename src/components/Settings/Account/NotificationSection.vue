2ea0bd214b8da084871fa4dba86e917d97c3b8ac
Bangun Bagustapa <bangunbagustapa@gmail.com>
2022-03-10 11:52:25 +0700

    feat: add notification

---
+++ src/components/Settings/Account/NotificationSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="px-6 py-4 bg-white rounded-lg">
+    <h2 class="text-xl text-green-700 font-medium">
+      Notifikasi
+    </h2>
+    <div class="flex justify-between items-center py-4">
+      <div class="flex flex-col flex-grow gap-2">
+        <p class="text-blue-gray-300">
+          Kirim ke Email
+        </p>
+        <p class="font-lato text-sm text-blue-gray-800">
+          Anda bersedia menerima pemberitahuan lewat email.
+        </p>
+      </div>
+      <JdsToggle checked />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
--- src/components/Settings/Account/index.vue
+++ src/components/Settings/Account/index.vue
@@ -1,16 +1,19 @@
 <template>
   <main class="space-y-4 pb-4">
     <PersonalInformationSection />
+    <NotificationSection />
   </main>
 </template>
 
 <script>
 import PersonalInformationSection from './PersonalInformationSection';
+import NotificationSection from './NotificationSection';
 
 export default {
   name: 'AccountSettings',
   components: {
     PersonalInformationSection,
+    NotificationSection,
   },
 };
 </script>
