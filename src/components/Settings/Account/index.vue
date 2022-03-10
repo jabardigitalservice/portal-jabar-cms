3f320aa96b6fe17e8cb503789952fbbcffd0ea94
Bangun Bagustapa <bangunbagustapa@gmail.com>
2022-03-10 15:36:06 +0700

    feat: add notification (#109)
    * feat: add notification
    
    * chore: remove unused code

--- src/components/Settings/Account/index.vue
+++ src/components/Settings/Account/index.vue
@@ -1,18 +1,21 @@
 <template>
   <main class="space-y-4 pb-4">
     <PersonalInformationSection />
+    <NotificationSection />
     <PrivacyAndSecuritySection />
   </main>
 </template>
 
 <script>
 import PersonalInformationSection from './PersonalInformationSection';
+import NotificationSection from './NotificationSection';
 import PrivacyAndSecuritySection from './PrivacyAndSecuritySection';
 
 export default {
   name: 'AccountSettings',
   components: {
     PersonalInformationSection,
+    NotificationSection,
     PrivacyAndSecuritySection,
   },
 };
