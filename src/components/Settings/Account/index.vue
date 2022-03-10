475b06f4f3bac0d152f3147702023c4c125a1e8d
Bangun Bagustapa <bangunbagustapa@gmail.com>
2022-03-10 13:09:44 +0700

    feat: password validation (#107)
    * feat: add password validation
    
    * feat: add tooltip
    
    * style: fix height

--- src/components/Settings/Account/index.vue
+++ src/components/Settings/Account/index.vue
@@ -1,16 +1,19 @@
 <template>
   <main class="space-y-4 pb-4">
     <PersonalInformationSection />
+    <PrivacyAndSecuritySection />
   </main>
 </template>
 
 <script>
 import PersonalInformationSection from './PersonalInformationSection';
+import PrivacyAndSecuritySection from './PrivacyAndSecuritySection';
 
 export default {
   name: 'AccountSettings',
   components: {
     PersonalInformationSection,
+    PrivacyAndSecuritySection,
   },
 };
 </script>
