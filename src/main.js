cc90f2713ae09a4deb588c187bf44832743a775f
Ida Bagus Wedagama <33661143+Ibwedagama@users.noreply.github.com>
2022-03-11 10:15:10 +0800

    chore: setup sentry (#113)
    * chore (packages): add sentry
    
    * chore: setup sentry performance and error tracking
    
    * fix (sentry): remove origin wildcard

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import * as Sentry from '@sentry/vue';
 import { DesignSystem } from '@jabardigitalservice/jds-design-system';
+import { BrowserTracing } from '@sentry/tracing';
 import PortalVue from 'portal-vue';
-import toast from '@/plugins/toast';
 import { axiosInterceptors } from './repositories/Repository';
+import toast from '@/plugins/toast';
 import router from './router';
 import store from './store';
 import App from './App';
@@ -19,6 +21,21 @@ if (process.env.VUE_APP_ENV === 'local') {
   const { worker } = require('./mocks/browser');
   worker.start();
 }
+
+Sentry.init({
+  Vue,
+  dsn: process.env.VUE_APP_SENTRY_DSN,
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      // NOTE: this origins should not be hardcoded
+      // TODO: find a way to make this values dynamic
+      tracingOrigins: ['develop--portal-jabar-cms.netlify.app'],
+    }),
+  ],
+  tracesSampleRate: parseFloat(process.env.VUE_APP_SENTRY_SAMPLE_RATE) || 0.2,
+  environment: process.env.VUE_APP_ENV || 'development',
+});
 
 store.dispatch('auth/getUser')
   .then(() => {
