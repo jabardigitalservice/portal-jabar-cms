bbce2a95b253a3a3c5e872f002fc963d001f6e58
Bangun Bagustapa <bangunbagustapa@gmail.com>
2022-03-17 11:39:30 +0700

    fix(route): fix route guard conditional (#132)

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ router.beforeEach((to, from, next) => {
   const publicRoute = to.matched.some((record) => record.meta.public);
 
   if (privateRoute && !isAuthenticated) next('/login');
-  if (publicRoute && isAuthenticated) next('/');
+  else if (publicRoute && isAuthenticated) next('/');
   else next();
 });
 
