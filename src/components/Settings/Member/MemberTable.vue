1070c4898338d0e4ac41d8515c3e68dbcc0e22e9
Ibwedagama <wardana.wedagama@gmail.com>
2022-03-18 12:50:22 +0800

    chore (member): add member-table and table-action component

---
+++ src/components/Settings/Member/MemberTable.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="member-table rounded-lg overflow-hidden border border-gray-100">
+    <JdsDataTable
+      :headers="tableHeader"
+      :items="items"
+      :loading="loading"
+      :pagination="pagination"
+      empty-text="Data tidak tersedia"
+      @next-page="onPaginationChange('next-page', $event)"
+      @previous-page="onPaginationChange('previous-page', $event)"
+      @per-page-change="onPaginationChange('per-page-change', $event)"
+      @page-change="onPaginationChange('page-change', $event)"
+      @change:sort="onSortChange($event)"
+    >
+      <!-- TODO: handle action button click -->
+      <!-- eslint-disable-next-line vue/valid-v-slot -->
+      <template #item.action="{item}">
+        <MemberTableAction
+          :item="item"
+        />
+      </template>
+    </JdsDataTable>
+  </div>
+</template>
+
+<script>
+import MemberTableAction from '@/components/Settings/Member/MemberTableAction';
+import { MEMBER_TABLE_HEADER } from '@/common/constants';
+
+export default {
+  name: 'NewsTable',
+  components: {
+    MemberTableAction,
+  },
+  props: {
+    items: {
+      type: Array,
+      require: false,
+      default: () => [],
+    },
+    loading: {
+      type: Boolean,
+      require: false,
+      default: false,
+    },
+    meta: {
+      type: Object,
+      require: false,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+      tableHeader: MEMBER_TABLE_HEADER,
+      pagination: {
+        currentPage: 1,
+        itemsPerPage: 10,
+        totalRows: 0,
+        itemsPerPageOptions: [10, 20, 30, 40, 50],
+      },
+    };
+  },
+  watch: {
+    meta: {
+      handler() {
+        this.pagination = {
+          ...this.pagination,
+          currentPage: this.meta?.current_page || 1,
+          itemsPerPage: this.meta?.per_page || 10,
+          totalRows: this.meta?.total_count || 0,
+        };
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    onPaginationChange(action, value) {
+      const paginationObj = { ...this.pagination };
+
+      switch (action) {
+        case 'next-page':
+        case 'previous-page':
+        case 'page-change':
+          paginationObj.currentPage = value;
+          break;
+
+        case 'per-page-change':
+          paginationObj.itemsPerPage = value;
+          break;
+
+        default:
+          break;
+      }
+
+      this.pagination = { ...paginationObj };
+
+      /**
+       *  NOTE:
+       *  `jds-pagination` emits `per-page-change` and `page-change` events
+       *  whenever user changes per page value.
+       *
+       *  To avoid double fetch, we immediately stop this function on
+       *  `per-page-change` event and let `page-change` event to
+       *  fetch data from API
+       */
+
+      if (action === 'per-page-change') {
+        return;
+      }
+
+      this.$emit('update:pagination', {
+        page: this.pagination.currentPage,
+        per_page: this.pagination.itemsPerPage,
+      });
+    },
+
+    onSortChange(sort) {
+      this.$emit('change:sort', {
+        sort_by: Object.keys(sort)[0],
+        sort_order: Object.values(sort)[0].toUpperCase(),
+      });
+    },
+  },
+};
+</script>
+
+<style>
+/**
+ * Override default jds-data-table style
+ */
+.member-table .jds-data-table {
+  table-layout: fixed !important;
+}
+</style>
