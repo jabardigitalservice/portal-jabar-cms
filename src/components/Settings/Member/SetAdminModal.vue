5efdbf9d072943a5cfd217720ca4b2afa1db9dd3
Ibwedagama <wardana.wedagama@gmail.com>
2022-03-24 11:26:20 +0800

    refactor: improve set-admin-modal

--- src/components/Settings/Member/SetAdminModal.vue
+++ src/components/Settings/Member/SetAdminModal.vue
@@ -12,21 +12,31 @@
         <br>
         Ketikkan Kata Sandi untuk konfirmasi.
       </p>
-      <em class="text-sm font-lato leading-6 text-blue-gray-800 mb-2">
-        *Kata sandi yang dimasukkan adalah kata sandi group admin
-      </em>
-      <div
-        class="w-full border border-blue-gray-100 rounded-lg overflow-hidden p-2
-      focus-within:border-green-600"
+      <form
+        class="flex flex-col flex-grow"
+        @submit.prevent="submitForm"
       >
-        <input
-          v-model="password"
-          placeholder="Masukkan kata sandi"
-          type="password"
-          class="text-sm text-center bg-white placeholder:text-blue-gray-100
-          w-full focus:outline-none leading-none"
+        <label
+          for="password"
+          class="text-sm font-medium text-blue-gray-800 italic mb-2"
         >
-      </div>
+          *Kata sandi yang dimasukkan adalah kata sandi group admin
+        </label>
+        <input
+          id="password"
+          v-model="password"
+          type="password"
+          class="focus:outline-none border p-2 rounded-md font-lato text-sm text-blue-gray-800"
+          :class="[isError ? 'bg-red-50 border-red-700' : 'bg-white border-gray-400 focus:border-green-700']"
+          placeholder="Masukkan kata sandi"
+        >
+        <p
+          class="text-red-600 text-xs mt-1"
+          :class="isError ? 'visible' : 'invisible'"
+        >
+          Kata sandi yang anda masukkan belum sesuai
+        </p>
+      </form>
     </div>
     <template #footer>
       <div class="flex w-full h-full items-center justify-end gap-4">
@@ -38,8 +48,17 @@
         </BaseButton>
         <BaseButton
           class="bg-green-700 hover:bg-green-600 text-sm text-white"
+          :disabled="isLoading || !isFormValid"
+          @click="submitForm"
         >
           Jadikan Administrator
+          <template #icon-right>
+            <JdsSpinner
+              v-show="isLoading"
+              size="16px"
+              foreground="#757575"
+            />
+          </template>
         </BaseButton>
       </div>
     </template>
@@ -69,11 +88,30 @@ export default {
   data() {
     return {
       password: '',
+      isLoading: false,
+      isError: false,
     };
+  },
+  computed: {
+    isFormValid() {
+      return this.password !== '';
+    },
   },
   methods: {
     closeModal() {
       this.$emit('close');
+      this.resetForm();
+    },
+    resetForm() {
+      this.password = '';
+    },
+    submitForm() {
+      if (this.isFormValid && !this.isLoading) {
+        this.handleSetAdmin();
+      }
+    },
+    async handleSetAdmin() {
+      // TODO: add set as admin functionality
     },
   },
 };
