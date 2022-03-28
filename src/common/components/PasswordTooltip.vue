9d4f55f29f6c07dc63e23424bc33ad9850bf0245
Bangun Bagustapa <bangunbagustapa@gmail.com>
2022-03-28 15:09:12 +0700

    refactor(password tooltip): move the tooltip into a seperate component (#151)
    * refactor(password tooltip): move the tooltip into a seperated component
    
    * chore(tooltip): add component name

---
+++ src/common/components/PasswordTooltip.vue
@@ -0,0 +1,84 @@
+<template>
+  <Tooltip :show="show">
+    <div class="flex gap-2 mb-2">
+      <div
+        class="h-1 rounded-lg w-14"
+        :class="[lowBarClassName]"
+      />
+      <div
+        class="h-1 rounded-lg w-14"
+        :class="[mediumBarClassName]"
+      />
+      <div
+        class="h-1 rounded-lg w-14"
+        :class="[strongBarClassName]"
+      />
+    </div>
+    <p>Kata sandi Anda <span :class="passwordStrengthLabelClassName">{{ passwordStrength.label }}</span></p>
+  </Tooltip>
+</template>
+
+<script>
+import Tooltip from '@/common/components/Tooltip';
+
+export default {
+  name: 'PasswordTooltip',
+  components: {
+    Tooltip,
+  },
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+    },
+    passwordStrength: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    lowBarClassName() {
+      switch (this.passwordStrength.type) {
+        case 'low':
+          return 'bg-red-600';
+        case 'medium':
+          return 'bg-yellow-600';
+        case 'strong':
+          return 'bg-green-600';
+        default:
+          return 'bg-gray-600';
+      }
+    },
+    mediumBarClassName() {
+      switch (this.passwordStrength.type) {
+        case 'medium':
+          return 'bg-yellow-600';
+        case 'strong':
+          return 'bg-green-600';
+        default:
+          return 'bg-gray-600';
+      }
+    },
+    strongBarClassName() {
+      switch (this.passwordStrength.type) {
+        case 'strong':
+          return 'bg-green-600';
+        default:
+          return 'bg-gray-600';
+      }
+    },
+    passwordStrengthLabelClassName() {
+      switch (this.passwordStrength.type) {
+        case 'low':
+          return 'text-red-500';
+        case 'medium':
+          return 'text-yellow-500';
+        case 'strong':
+          return 'text-green-500';
+        default:
+          return '';
+      }
+    },
+  },
+};
+</script>
---
+++ src/common/components/Tooltip.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    v-show="show"
+    class="absolute mb-2"
+  >
+    <div class="bg-gray-900 p-3 top-0 left-0 rounded-lg text-white text-xs">
+      <slot />
+    </div>
+    <div class="absolute top-1 left-3 w-3 h-3 -mt-2 rotate-45 bg-gray-900" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tooltip',
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
