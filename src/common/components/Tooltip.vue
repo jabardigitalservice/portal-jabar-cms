61f81497c6279a9089c7c26ab491bbb62ef4d19c
Bangun Bagustapa <bangunbagustapa@gmail.com>
2022-03-28 11:58:00 +0700

    refactor(password tooltip): move the tooltip into a seperated component

---
+++ src/common/components/Tooltip.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    v-show="show"
+    class="absolute mb-2"
+  >
+    <div class="bg-gray-900 p-3 top-0 left-0 rounded-lg text-white text-xs">
+      <slot />
+    </div>
+    <div class="absolute top-1 left-3 w-3 h-3 -mt-2 rotate-45 bg-gray-900" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
