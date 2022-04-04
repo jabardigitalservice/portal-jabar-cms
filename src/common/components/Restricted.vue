7177decea3e72e6044fc7331613d30070d0eae6b
Ida Bagus Wedagama <33661143+Ibwedagama@users.noreply.github.com>
2022-04-04 16:23:19 +0700

    feat: permissions (#165)
    * chore (permissions): add get-permissions method on auth-repository
    
    * chore (mock): add mock response for user and permissions endpoint
    
    * refactor (store/auth): get permissions data on get-user action; add permissions getter
    
    * chore: create restricted component
    
    * fix (restricted): fix component name

---
+++ src/common/components/Restricted.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="isPermitted">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'Restricted',
+  props: {
+    permission: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    ...mapGetters('auth', ['permissions']),
+    isPermitted() {
+      if (Array.isArray(this.permissions) && this.permissions.length) {
+        return this.permissions.includes(this.permission);
+      }
+      return false;
+    },
+  },
+
+};
+</script>
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -4,28 +4,80 @@ import { rest } from 'msw';
 const BASE_URL = process.env.VUE_APP_API_URL;
 
 const handlers = [
-  rest.post(`${BASE_URL}/v1/auth/login`, (req, res, ctx) => res(
-    ctx.status(401),
-  )),
-
   // rest.post(`${BASE_URL}/v1/auth/login`, (req, res, ctx) => res(
-  //   ctx.status(200),
+  //   ctx.status(401),
   //   ctx.json({
-  //     access_token: 'asdasdasd',
-  //     refresh_token: '123123123',
-  //     exp: '123123',
+  //     message: 'Invalid credentials',
   //   }),
   // )),
 
-  // rest.get(`${BASE_URL}/v1/auth/me`, (req, res, ctx) => res(
-  //   ctx.status(200),
+  rest.post(`${BASE_URL}/v1/auth/login`, (req, res, ctx) => res(
+    ctx.status(200),
+    ctx.json({
+      access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+      refresh_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+      exp: 1649047862,
+    }),
+  )),
+
+  // rest.get(`${BASE_URL}/v1/users/me`, (req, res, ctx) => res(
+  //   ctx.status(500),
+  // )),
+
+  rest.get(`${BASE_URL}/v1/users/me`, (req, res, ctx) => res(
+    ctx.status(200),
+    ctx.json({
+      data: {
+        id: 'b5a35743-2d4b-4559-939a-2ba21c06f39c',
+        name: 'Mock User',
+        username: 'mockuser',
+        email: 'mock@user.com',
+        nip: '123456789012345672',
+        occupation: 'Kepala Dinas',
+        photo: 'https://media.newyorker.com/photos/5f01e383b975762d612e0ff3/2:2/w_746,h_746,c_limit/Barasch-Avatar.jpg',
+        unit: {
+          id: 26,
+          name: 'Dinas Komunikasi dan Informatika',
+        },
+        role: {
+          id: 2,
+          name: 'Group Admin',
+        },
+        last_password_changed: null,
+      },
+    }),
+  )),
+
+  // rest.get(`${BASE_URL}/v1/auth/permissions`, (req, res, ctx) => res(
+  //   ctx.status(500),
   //   ctx.json({
   //     data: {
-  //       id: '121212',
-  //       username: 'bangun',
+  //       message: 'Invalid credentials',
   //     },
   //   }),
   // )),
+
+  rest.get(`${BASE_URL}/v1/auth/permissions`, (req, res, ctx) => res(
+    ctx.status(200),
+    ctx.json({
+      data: {
+        permissions: [
+          'event.manage',
+          'news.archive',
+          'news.manage',
+          'news.publish',
+          'user.cancel-invitation',
+          'user.change-email',
+          'user.deactivate',
+          'user.invite',
+          'user.manage',
+          'user.request-to-be-admin',
+          'user.set-as-admin',
+        ],
+      },
+    }),
+  )),
+
 ];
 
 export { handlers };
